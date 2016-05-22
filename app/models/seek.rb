e16d6a3ac7cb431a4380ef7f293ccff4231b5cc5
jiunjiun <quietmes@gmail.com>
2016-05-22 16:39:23 +0800

    update seek

--- app/models/seek.rb
+++ app/models/seek.rb
@@ -2,7 +2,7 @@ class Seek
   def self.create(user_token)
     return if Room.find_by_user_token(user_token)
     if found_user_token = $redis.spop("seeks")
-      room_token = Digest::MD5.hexdigest("#{user_token}-#{found_user_token}")
+      room_token = Digest::MD5.hexdigest("#{Time.now.to_i}-#{user_token}-#{found_user_token}")
       Room.create(token: room_token)
 
       Rails.logger.debug { " -- room_#{room_token}" }
