2226011b049ad7fc00bd2bfce1bbd536601bfb9a
jiunjiun <quietmes@gmail.com>
2016-05-22 23:48:55 +0800

    logo add animation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def self_say?(message_user_token, whosay)
     message_user_token == whosay
   end
+
+  def random_image
+    Dir.new(Rails.root.to_s + "/app/assets/images/spaces").to_a.select{|f| f.downcase.match(/\.jpg|\.jpeg|\.png/) }.sample
+  end
 end
